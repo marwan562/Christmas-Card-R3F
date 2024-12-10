c06b779e396da03ff7ddbdc24ec8da8da08be6c3
marwan562 <mixing.gamer546@gmail.com>
2024-12-11 01:14:47 +0200

    first commit

---
+++ src/components/Interface.jsx
@@ -0,0 +1,20 @@
+
+export const Interface = () => {
+  return (
+    <>
+      <div className="interface">
+        <h1 className="interface__title">
+          ⭐️ for Christmas Ambiance / 👇 for Christmas Party
+        </h1>
+        <a
+          className="interface__link"
+          href="https://www.linkedin.com/in/marwan-hassan-18901a297/"
+          target="_blank"
+        >
+          Linkedin/@MarwanHassan
+        </a>
+      </div>
+      
+    </>
+  );
+};
